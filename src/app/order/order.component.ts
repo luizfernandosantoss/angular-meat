436cfc9b5d13a89a37de47766f82f42f29c781aa
Luiz Fernando Santos da Silva <luiz.santos@preventsenior.com.br>
2019-07-22 17:01:19 -0300

    Criando validação do e-mail utilizando o formGroup no arquivo ts

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,7 +4,7 @@ import {OrderService} from './order.service'
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model'
 import {Order, OrderItem} from './order.model'
 import {Router} from '@angular/router'
-import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'mt-order',
@@ -32,7 +32,21 @@ export class OrderComponent implements OnInit {
       optinalAndress: this.formBulder.control(''),
       paymentOption: this.formBulder.control('',Validators.required)
 
-    })
+    },{validator: OrderComponent.equalsTo})
+  }
+
+  static equalsTo(group: AbstractControl): {[key:string]: boolean}{
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+    if(!email || !emailConfirmation){
+      return undefined
+    }
+
+    if(email.value !== emailConfirmation.value){
+      return {emailsNotMatch:true}
+    }
+    return undefined
+
   }
   delivery:number = 8
 
