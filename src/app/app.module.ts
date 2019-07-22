1c2921b75cb3d0590c3c14d0e8afa6247fa822ad
Luiz Fernando Santos da Silva <luiz.santos@preventsenior.com.br>
2019-07-22 14:42:24 -0300

    Iniciando a refatoração trabalhando com reactForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser'
 import {LOCALE_ID, NgModule} from '@angular/core'
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import {HttpModule} from '@angular/http'
+import {RouterModule} from '@angular/router'
 
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component'
-import {ROUTES} from "./app.routes";
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import {RestaurantsService} from './restaurants/restaurants.service';
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import {AppComponent} from './app.component'
+import {HeaderComponent} from './header/header.component'
+import {FooterComponent} from './footer/footer.component'
+import {HomeComponent} from './home/home.component'
+import {AboutComponent} from './about/about.component'
+import {ROUTES} from './app.routes'
+import {RestaurantsComponent} from './restaurants/restaurants.component'
+import {RestaurantComponent} from './restaurants/restaurant/restaurant.component'
+import {RestaurantsService} from './restaurants/restaurants.service'
+import {RestaurantDetailComponent} from './restaurant-detail/restaurant-detail.component'
+import {MenuComponent} from './restaurant-detail/menu/menu.component'
+import {ShoppingCartComponent} from './restaurant-detail/shopping-cart/shopping-cart.component'
+import {MenuItemComponent} from './restaurant-detail/menu-item/menu-item.component'
+import {ReviewsComponent} from './restaurant-detail/reviews/reviews.component'
 import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-cart.service'
-import {FormsModule} from '@angular/forms';
-import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shered/input/input.component';
-import { RadioComponent } from './shered/radio/radio.component';
-import { OrderItensComponent } from './order/order-itens/order-itens.component'
-import {OrderService} from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
-import { OrderSummaryComponent } from './order/order-summary/order-summary.component';
-import { RatingComponent } from './shered/rating/rating.component'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {OrderComponent} from './order/order.component'
+import {InputComponent} from './shered/input/input.component'
+import {RadioComponent} from './shered/radio/radio.component'
+import {OrderItensComponent} from './order/order-itens/order-itens.component'
+import {OrderService} from './order/order.service'
+import {DeliveryCostsComponent} from './order/delivery-costs/delivery-costs.component'
+import {OrderSummaryComponent} from './order/order-summary/order-summary.component'
+import {RatingComponent} from './shered/rating/rating.component'
 
 
 @NgModule({
@@ -51,14 +51,19 @@ import { RatingComponent } from './shered/rating/rating.component'
     DeliveryCostsComponent,
     OrderSummaryComponent,
     RatingComponent,
+
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [RestaurantsService,ShoppingCartService,OrderService,{provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [RestaurantsService,
+    ShoppingCartService,
+    OrderService,
+    {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,6 +4,7 @@ import {OrderService} from './order.service'
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model'
 import {Order, OrderItem} from './order.model'
 import {Router} from '@angular/router'
+import {FormBuilder, FormGroup} from '@angular/forms'
 
 @Component({
   selector: 'mt-order',
@@ -16,11 +17,24 @@ export class OrderComponent implements OnInit {
       {label: "Cartão de Débito", value: "DEB"},
       {label: "Cartão Refeição", value: "REF"}
   ]
-  constructor(private orderService: OrderService, private  router: Router) { }
+  constructor(private orderService: OrderService, private  router: Router, private formBulder: FormBuilder) { }
+
+  orderForm: FormGroup
 
   ngOnInit() {
+    this.orderForm = this.formBulder.group({
+      name: '',
+      email: this.formBulder.control(''),
+      emailConfirmation: this.formBulder.control(''),
+      andress: this.formBulder.control(''),
+      number: this.formBulder.control(''),
+      optinalAndress: this.formBulder.control(''),
+      paymentOption: this.formBulder.control('')
+
+    })
   }
   delivery:number = 8
+
   itemsValue(): number {
     return this.orderService.itemsValue()
   }
