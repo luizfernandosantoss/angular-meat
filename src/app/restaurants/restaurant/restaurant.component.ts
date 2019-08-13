a5a32ea62bf34b4bbc52c29aa088a2494d0c2d07
Luiz Fernando Santos da Silva <luiz.santos@preventsenior.com.br>
2019-08-13 18:37:05 -0300

    Atualizando a tela de restaurant, colocando uma animação

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,15 +1,24 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 import {Restaurant} from './restaurant.model';
+import {animate, state, style, transition, trigger} from '@angular/animations'
 @Component({
   selector: 'mt-restaurant',
-  templateUrl: './restaurant.component.html'
+  templateUrl: './restaurant.component.html',
+  animations:[
+    trigger('restaurantAppeared',[
+      state('ready',style({opacity:1})),
+      transition('void => ready',[
+          style({opacity:0,transform:'translate(-30px, -10px)'}),
+          animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class RestaurantComponent implements OnInit {
-
+  restaurantState = 'ready'
+  @Input() restaurant: Restaurant
   constructor() { }
-
-  @Input() restaurant: Restaurant;
 
   ngOnInit() {
   }
